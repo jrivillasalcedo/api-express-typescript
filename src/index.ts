9035843a50ae6717f3bc5836c30af80dbc2db3eb
jrivilla <jrivillarepository@gmail.com>
2023-06-02 19:40:23 +0200

    feat: Setup Express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log("Hello typescript");
+import express from "express";
+import http from "http";
+import bodyPaser from "body-parser";
+import cookieParse from "cookie-parser";
+import compression from "compression";
+import cors from "cors";
+
+const app = express();
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+
+app.use(compression());
+app.use(cookieParse());
+app.use(bodyPaser.json());
+
+const server = http.createServer(app);
+
+server.listen(8080, () => {
+  console.log("Server running on http://localhost:8080/");
+});
