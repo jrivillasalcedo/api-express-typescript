67d4a27da4af8c4662d9619791a2fcf68d68232c
jrivilla <jrivillarepository@gmail.com>
2023-06-02 20:04:32 +0200

    feat: MongoDB Setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ import bodyPaser from "body-parser";
 import cookieParse from "cookie-parser";
 import compression from "compression";
 import cors from "cors";
+import mongoose from "mongoose";
+
+// needed dependency for read .env file
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(
@@ -21,3 +26,10 @@ const server = http.createServer(app);
 server.listen(8080, () => {
   console.log("Server running on http://localhost:8080/");
 });
+
+let password = process.env.DATABASE_PASSWORD;
+const MONGO_URL = `mongodb+srv://jrivillarepository:${password}@cluster0.3o0fslw.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on("error", (error: Error) => console.log(error));
